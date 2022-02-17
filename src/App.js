55c072f6cbbbe22650400a9dbf9b846994796ad7
Jack Cruzado Ocaris <jackco83@hotmail.com>
2022-02-17 14:51:19 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//import React from 'react'
+
+//const App = () => "Hola Mundo con App Importado"
+
+//CommonJS : module.exports = App
+//export default App
+//import React  from 'react'
+
+import Header from "./Header"
+import Footer from "./Footer"
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
+
+export default App
